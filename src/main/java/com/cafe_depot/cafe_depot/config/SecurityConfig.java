d3edd8c3c290ec07f1a3eb9eb5c4b411b164a60c
Shakhram <shakag6@gmail.com>
2024-01-08 22:34:16 -0500

    Edit security config to include cors

--- src/main/java/com/cafe_depot/cafe_depot/config/SecurityConfig.java
+++ src/main/java/com/cafe_depot/cafe_depot/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.cafe_depot.cafe_depot.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class SecurityConfig {
@@ -12,7 +16,15 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // http.authorizeHttpRequests().anyRequest().permitAll();
-        http.csrf(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Replace with your frontend
+                    config.setAllowedMethods(Arrays.asList("*")); // You can specify specific methods if needed
+                    config.setAllowedHeaders(Arrays.asList("*")); // You can specify specific headers if needed
+                    return config;
+                }));
+
         return http.build();
     }
 }
